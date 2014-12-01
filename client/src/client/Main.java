1279a4e3b0b25b88d004310eafb09affba1f8583
gixslayer <ciske_harsema_2@hotmail.com>
2014-12-01 12:31:17 +0100

    Basic network test.

--- client/src/client/Main.java
+++ client/src/client/Main.java
@@ -1,30 +1,41 @@
 package client;
 
-import findfour.shared.events.Event;
-import findfour.shared.events.EventDispatcher;
 import findfour.shared.events.EventHandler;
-import findfour.shared.events.InvalidEventHandlerException;
+import findfour.shared.network.Packet;
+import findfour.shared.network.PacketTest;
+import findfour.shared.network.TcpClient;
 
 public class Main {
 
-    private static EventDispatcher dispatcher;
+    private static TcpClient client = new TcpClient();
 
-    public static void main(String[] args) {
-        dispatcher = new EventDispatcher();
-        dispatcher.registerEvent(new Event(0, String.class, String.class, int.class));
-        try {
-            dispatcher.registerEventHandlers(new Main());
+    public static void main(String[] args) throws InterruptedException {
+        client.registerEventHandlers(new Main());
+        client.connect("127.0.0.1", 666, 1000);
 
-            dispatcher.raiseEvent(0, "Hello", "Bob", 4);
-        } catch (InvalidEventHandlerException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+        while (client.isConnected()) {
+            Thread.sleep(100);
         }
     }
 
-    @EventHandler(eventId = 0)
-    private void testHandler(String arg1, String arg2, int a) {
-        System.out.println(arg1 + arg2);
-        System.out.println(a);
+    @EventHandler(eventId = TcpClient.EVENT_CONNECTED)
+    private void connected() {
+        System.out.println("Connected");
+    }
+
+    @EventHandler(eventId = TcpClient.EVENT_DISCONNECTED)
+    private void disconnected() {
+        System.out.println("Disconnected");
+    }
+
+    @EventHandler(eventId = TcpClient.EVENT_PACKET_RECEIVED)
+    private void packetReceived(Packet packet, int packetSize) {
+        System.out.println("Received packet");
+        System.out.printf("Size: %d\n", packetSize);
+        PacketTest packetTest = (PacketTest) packet;
+
+        System.out.println("Message: " + packetTest.getData());
+
+        client.send(packetTest);
     }
 }
--- server/src/server/Main.java
+++ server/src/server/Main.java
@@ -1,10 +1,53 @@
 package server;
 
+import findfour.shared.events.EventHandler;
+import findfour.shared.network.Packet;
+import findfour.shared.network.PacketTest;
+import findfour.shared.network.TcpServer;
+
 public class Main {
+    private static TcpServer server = new TcpServer();
 
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
+    public static void main(String[] args) throws InterruptedException {
+        server.registerEventHandlers(new Main());
+        server.start(666);
 
+        while (server.isRunning()) {
+            Thread.sleep(100);
+        }
     }
 
+    @EventHandler(eventId = TcpServer.EVENT_STARTED)
+    private void started(int port) {
+        System.out.printf("Started on port %d\n", port);
+    }
+
+    @EventHandler(eventId = TcpServer.EVENT_STOPPED)
+    private void stopped() {
+        System.out.println("Stopped");
+    }
+
+    @EventHandler(eventId = TcpServer.EVENT_CLIENT_CONNECTED)
+    private void clientConnected(int clientId, String host) {
+        System.out.printf("Client connected (%d) from %s\n", clientId, host);
+
+        PacketTest packet = new PacketTest();
+        packet.setData(String.format("Hello, %d", clientId));
+
+        server.send(clientId, packet);
+    }
+
+    @EventHandler(eventId = TcpServer.EVENT_CLIENT_DISCONNECTED)
+    private void clientDisconnected(int clientId) {
+        System.out.printf("Client %d disconnected\n", clientId);
+    }
+
+    @EventHandler(eventId = TcpServer.EVENT_PACKET_RECEIVED)
+    private void packetReceived(int clientId, Packet packet, int packetSize) {
+        System.out.printf("Packet from %d (%d bytes)\n", clientId, packetSize);
+
+        PacketTest packetTest = (PacketTest) packet;
+
+        System.out.println("Message: " + packetTest.getData());
+    }
 }
