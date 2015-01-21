6e6d2f2ee81a022aa60d87253a911a9913b809dc
gixslayer <ciske_harsema_2@hotmail.com>
2015-01-22 00:24:09 +0100

    Synchronized log methods for thread safety.

--- shared/src/findfour/shared/logging/ConsoleListener.java
+++ shared/src/findfour/shared/logging/ConsoleListener.java
@@ -3,17 +3,17 @@ package findfour.shared.logging;
 public class ConsoleListener implements LogListener {
 
     @Override
-    public void info(String message) {
+    public synchronized void info(String message) {
         System.out.println(message);
     }
 
     @Override
-    public void warning(String message) {
+    public synchronized void warning(String message) {
         System.out.printf("WARNING: %s\n", message);
     }
 
     @Override
-    public void error(String message) {
+    public synchronized void error(String message) {
         System.err.println(message);
     }
 
