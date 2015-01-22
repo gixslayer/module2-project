242e89b7ebe9c85a7b5e9199240ae5067f5ab118
gixslayer <ciske_harsema_2@hotmail.com>
2015-01-22 01:37:02 +0100

    Added logging code.

--- server/src/server/rooms/RoomManager.java
+++ server/src/server/rooms/RoomManager.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 import server.player.Player;
+import findfour.shared.logging.Log;
+import findfour.shared.logging.LogLevel;
 
 public final class RoomManager {
     private final Lobby lobby;
@@ -19,7 +21,7 @@ public final class RoomManager {
     }
 
     public void startGame(Player playerA, Player playerB) {
-        String roomName = String.format("%svs%s", playerA.getName(), playerB.getName());
+        String roomName = String.format("%s:%s", playerA.getName(), playerB.getName());
         GameRoom room = new GameRoom(roomName, playerA, playerB);
 
         playerA.moveToGame(room);
@@ -27,14 +29,23 @@ public final class RoomManager {
 
         gameRooms.put(roomName, room);
 
+        Log.info(LogLevel.Normal, "Starting game between %s and %s", playerA.getName(),
+                playerB.getName());
+
         room.begin();
     }
 
     void endGame(GameRoom room) {
+        String roomName = room.getName();
+        String playerA = roomName.substring(0, roomName.indexOf(':'));
+        String playerB = roomName.substring(roomName.indexOf(':') + 1);
+
+        Log.info(LogLevel.Normal, "The game between %s and %s has ended", playerA, playerB);
+
         for (Player player : room.getPlayers()) {
             player.moveToLobby();
         }
 
-        gameRooms.remove(room.getName());
+        gameRooms.remove(roomName);
     }
 }
