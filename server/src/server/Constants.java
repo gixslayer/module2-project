81f7644a5971cbcaea89fdf964ee24e096eb9648
gixslayer <ciske_harsema_2@hotmail.com>
2015-01-22 01:16:44 +0100

    Added JavaDoc.

--- server/src/server/Constants.java
+++ server/src/server/Constants.java
@@ -1,10 +1,29 @@
 package server;
 
+/**
+ * Defines a set of constants that can be referred throughout the code-base.
+ * @author ciske
+ *
+ */
 public final class Constants {
+    /**
+     * The name of the group that programmed this server.
+     */
     public static final String GROUP = "19";
-    // TODO: Fill in extensions and actually support them.
+    /**
+     * The space separated list of extensions supported by this server.
+     */
     public static final String SUPPORTED_EXTENSIONS = "Lobby Chat Challenge";
+    /**
+     * The name of the lobby extension as described in the protocol.
+     */
     public static final String EXT_LOBBY = "Lobby";
+    /**
+     * The name of the chat extension as described in the protocol.
+     */
     public static final String EXT_CHAT = "Chat";
+    /**
+     * The name of the challenge extension as described in the protocol.
+     */
     public static final String EXT_CHALLENGE = "Challenge";
 }
